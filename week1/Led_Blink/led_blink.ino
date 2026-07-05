150bc3278690b3cf27b28d095255d2d7a8388941
Prathamesh P Naik <prathmeshnaik.ictd25@adaniuni.ac.in>
2026-07-05 13:22:16 +0530

    feat: implement a counter and print blink count to serial

--- week1/Led_Blink/led_blink.ino
+++ week1/Led_Blink/led_blink.ino
@@ -1,10 +1,17 @@
+int blinkCount = 0; // Initialize counter
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600); // Start serial communication
 }
 
 void loop() {
   digitalWrite(13, HIGH);
-  delay(500); // Changed to 500ms
+  delay(500);
   digitalWrite(13, LOW);
-  delay(500); // Changed to 500ms
+  delay(500);
+
+  blinkCount++;
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount);
 }
